7853a91c6f9033478030c0f0cf35868c955ccfc4
Wei Wang <weiwio@outlook.com>
2018-10-17 13:38:26 -0700

    Put ul outside p

--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -5,15 +5,14 @@ export default () => (
     <div className='major'>
       <h1>Next.js + Electron.js</h1>
       <h2>Create apps, the morden way</h2>
-      <p>Check the following if you are<br />
-        <ul>
-          <li>new to JavaScript: <a href='https://javascript.info/'>The Modern JavaScript Tutorial</a></li>
-          <li>new to Node.js: <a href='https://nodeschool.io/'>Node School</a></li>
-          <li>new to React.js: <a href='https://reactjs.org/tutorial/tutorial.html/'>React Tutorial</a></li>
-          <li>new to Next.js: <a href='https://nextjs.org/learn/'>Learn Next.js</a></li>
-          <li>new to Electron.js: <a href='https://github.com/electron/electron-api-demos'>Electron API Demos</a></li>
-        </ul>
-      </p>
+      <p>Check the following if you are</p>
+      <ul>
+        <li>new to JavaScript: <a href='https://javascript.info/'>The Modern JavaScript Tutorial</a></li>
+        <li>new to Node.js: <a href='https://nodeschool.io/'>Node School</a></li>
+        <li>new to React.js: <a href='https://reactjs.org/tutorial/tutorial.html/'>React Tutorial</a></li>
+        <li>new to Next.js: <a href='https://nextjs.org/learn/'>Learn Next.js</a></li>
+        <li>new to Electron.js: <a href='https://github.com/electron/electron-api-demos'>Electron API Demos</a></li>
+      </ul>
     </div>
     <style jsx>{`
       .major {
